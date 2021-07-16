2a7e932f6336f04217d33648c543cad2751847c9
Luis Velásquez <luvejo.dev@gmail.com>
2021-07-15 21:14:50 -0500

    feat: add initial carousel component

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Carousel />
+</template>
+
+<script>
+import Carousel from './components/Carousel.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Carousel
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #656565;
+  margin-top: 10px;
+}
+</style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="carousel">
+    <div class="inner">
+      <div class="card" v-for="card in cards" :key="card">
+        {{ card }}
+      </div>
+    </div>
+  </div>
+  <button>prev</button>
+  <button>next</button>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      cards: [1, 2, 3, 4, 5, 6, 7, 8]
+    }
+  }
+}
+</script>
+
+<style>
+.carousel {
+  width: 170px;
+  overflow: hidden;
+
+  /* optional */
+  margin-bottom: 10px;
+}
+
+.inner {
+  transition: transform 0.2s;
+  white-space: nowrap;
+}
+
+.card {
+  width: 40px;
+  margin-right: 10px;
+  display: inline-flex;
+
+  /* optional */
+  height: 40px;
+  border-radius: 4px;
+  background-color: #b0b0b0;
+  align-items: center;
+  justify-content: center;
+}
+</style>
